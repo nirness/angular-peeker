7064d68b6d8c8c8f294f68079a147cf7f52df5ec
aviv <shaked.aviv1@gmail.com>
2014-09-05 23:18:29 +0300

    chore(app): Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularPeekerApp
+ * @description
+ * # angularPeekerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularPeekerApp', []);
