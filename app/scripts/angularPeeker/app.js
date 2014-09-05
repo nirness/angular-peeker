5e0eb42f328e33f78b6e0415eb6536096904ad30
aviv <shaked.aviv1@gmail.com>
2014-09-05 23:58:39 +0300

    feat(angularPeeker): Adds the main app's module

---
+++ app/scripts/angularPeeker/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularPeeker
+ * @description
+ * # angularPeeker
+ *
+ * Main module of the application.
+ */
+
+angular.module('angularPeeker', []);
