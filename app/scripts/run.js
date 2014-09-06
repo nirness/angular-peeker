3e47da1adba5f9f999ff15cc83401416fa9c402c
aviv <shaked.aviv1@gmail.com>
2014-09-06 10:18:16 +0300

    feat(run): Adds Event on $rootScope when peekerActivated

--- app/scripts/run.js
+++ app/scripts/run.js
@@ -9,6 +9,7 @@ angular.module('angularPeeker')
             logger.l('angularPeeker: run: $window', $window);
             logger.l('angularPeeker: run: config', config);
 
+            // Activates peeker on selected key
             $window.addEventListener('keydown', function (evt) {
                 var keyCode = config.peekerHotKey.key.toUpperCase().charCodeAt(0);
                 if ((evt.keyCode === keyCode) &&
@@ -16,8 +17,17 @@ angular.module('angularPeeker')
                     (evt.shiftKey === config.peekerHotKey.shiftKey) &&
                     (evt.ctrlKey === config.peekerHotKey.ctrlKey)) {
                     logger.l('Peeker Activated!');
+                    $rootScope.$broadcast('angularpeeker:run:peekeractivated');
                 }
             }, false);
+
+
+            // When peeker activetd
+            $rootScope.$on('angularpeeker:run:peekeractivated', function () {
+                var body = document.getElementsByTagName('body')[0];
+                logger.l('Add class \'peekerActivated\' to body', body);
+                angular.element(body).addClass('peekerActivated');
+            });
         }
     ]);
 
