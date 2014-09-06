91b3f46e6e6022421fb3b6591221326c51bf40c0
aviv <shaked.aviv1@gmail.com>
2014-09-06 18:35:43 +0300

    chore(dist): Updates dist

--- dist/scripts/angularPeeker.js
+++ dist/scripts/angularPeeker.js
@@ -16,34 +16,8 @@ angular.module('angularPeeker', []);
 // Source: app/scripts/run.js
 angular.module('angularPeeker')
     .run([
-        '$rootScope',
-        '$window',
-        'config',
-        'logger',
-        function ($rootScope, $window, config, logger) {
-            logger.l('angularPeeker: run: $rootScope', $rootScope);
-            logger.l('angularPeeker: run: $window', $window);
-            logger.l('angularPeeker: run: config', config);
-
-            // Activates peeker on selected key
-            $window.addEventListener('keydown', function (evt) {
-                var keyCode = config.peekerHotKey.key.toUpperCase().charCodeAt(0);
-                if ((evt.keyCode === keyCode) &&
-                    (evt.altKey === config.peekerHotKey.altKey) &&
-                    (evt.shiftKey === config.peekerHotKey.shiftKey) &&
-                    (evt.ctrlKey === config.peekerHotKey.ctrlKey)) {
-                    logger.l('Peeker Activated!');
-                    $rootScope.$broadcast('angularpeeker:run:peekeractivated');
-                }
-            }, false);
-
-
-            // When peeker activetd
-            $rootScope.$on('angularpeeker:run:peekeractivated', function () {
-                var body = document.getElementsByTagName('body')[0];
-                logger.l('Add class \'peekerActivated\' to body', body);
-                angular.element(body).addClass('peekerActivated');
-            });
+        'peeker',
+        function (peeker) {
         }
     ]);
 
@@ -57,14 +31,43 @@ angular.module('angularPeeker')
  * Value in the angularPeeker.
  */
 angular.module('angularPeeker')
-  .value('config', {
-        peekerHotKey: {
-            key: 'p',
-            altKey: true,
-            shiftKey: false,
-            ctrlKey: true
+    .provider('config', [
+        function () {
+            var peekerHotKey = {
+                key: 'p',
+                altKey: true,
+                shiftKey: false,
+                ctrlKey: true
+            };
+
+            // Public API for configuration
+            this.setActivationKey = function (key) {
+                peekerHotKey.key = key;
+            };
+            this.setActivationKeyAlt = function (alt) {
+                peekerHotKey.altKey = alt;
+            };
+            this.setActivationKeyCtrl = function (ctrl) {
+                peekerHotKey.ctrlKey = ctrl;
+            };
+            this.setActivationKeyShift = function (shift) {
+                peekerHotKey.shiftKey = shift;
+            };
+            this.setActivationConfiguration = function (configObj) {
+                for (var key in configObj) {
+                    if (configObj.hasOwnProperty(key)) {
+                        peekerHotKey[key] = configObj[key];
+                    }
+                }
+            };
+
+            this.$get = function () {
+                return {
+                    peekerHotKey: peekerHotKey
+                };
+            };
         }
-    });
+    ]);
 
 // Source: app/scripts/services/logger.js
 /**
@@ -115,5 +118,168 @@ angular.module('angularPeeker')
             l: api.basicInfo
         };
     });
+
+// Source: app/scripts/services/peeker.js
+    /**
+     * @ngdoc service
+     * @name angularPeeker.peeker
+     * @description
+     * # peeker
+     * Provider in the angularPeeker.
+     */
+    angular.module('angularPeeker')
+        .provider('peeker', [
+            function () {
+
+                // Method for instantiating
+                this.$get = [
+                    '$rootScope',
+                    '$window',
+                    'config',
+                    'logger',
+                    function ($rootScope, $window, config, logger) {
+                        logger.l('angularPeeker: run: $rootScope', $rootScope);
+                        logger.l('angularPeeker: run: $window', $window);
+                        logger.l('angularPeeker: run: config', config);
+
+
+                        //===============================
+                        //      Private variables       =
+                        //===============================
+                        var peekerActivated = false;
+                        var body = document.getElementsByTagName('body')[0];
+                        var html = document.getElementsByTagName('html')[0];
+                        var eventListenersRemovers = [];
+
+                        //===============================
+                        //      Private Methods         =
+                        //===============================
+
+
+                        var getScope = function (elem) {
+                            return angular.element(elem).scope();
+                        };
+
+                        var removeEventListeners = function () {
+                            eventListenersRemovers.forEach(function (remover) {
+                                remover();
+                            });
+                            eventListenersRemovers = [];
+                        };
+
+                        var on = function (configObj) {
+                            configObj.obj = configObj.obj !== undefined ? configObj.obj : $window;
+                            configObj.apturePhase = configObj.capturePhase !== undefined ? configObj.capturePhase : false;
+                            configObj.obj.addEventListener(configObj.eventType, configObj.handler, configObj.capturePhase);
+
+                            var remover = function () {
+                                configObj.obj.removeEventListener(configObj.eventType, configObj.handler, configObj.capturePhase);
+                            };
+
+                            eventListenersRemovers.push(remover);
+
+                            return remover;
+                        };
+
+                        var activatePeeker = function () {
+
+                            $rootScope.$broadcast('angularpeeker:run:peekeractivated');
+
+                            logger.l('Add class \'angularpeeker_peekerActivated\' to html', body);
+                            angular.element(html).addClass('angularpeeker_peekerActivated');
+
+                            on({
+                                eventType: 'mouseover',
+                                handler: function (evt) {
+                                    angular.element(evt.srcElement).addClass('angularpeeker_elementHovered');
+                                },
+                                capturePahse: true
+                            });
+
+                            on({
+                                eventType: 'mouseout',
+                                handler: function (evt) {
+                                    angular.element(evt.srcElement).removeClass('angularpeeker_elementHovered');
+                                },
+                                capturePahse: true
+                            });
+
+                            on({
+                                eventType: 'click',
+                                handler: function (evt) {
+                                    evt.stopImmediatePropagation();
+                                    displayScope(evt);
+                                },
+                                capturePahse: true
+                            });
+
+                        };
+
+                        var deactivatePeeker = function () {
+                            $rootScope.$broadcast('angularpeeker:run:peekerdeactivated');
+
+                            logger.l('Remove class \'angularpeeker_peekerActivated\' to html', body);
+                            angular.element(html).removeClass('angularpeeker_peekerActivated');
+
+                            //remove all angularpeeker_elementHovered classes
+
+                            var elems = document.getElementsByClassName('angularpeeker_elementHovered');
+                            elems = Array.prototype.slice.call(elems);
+                            elems.forEach(function (elem) {
+                                angular.element(elem).removeClass('angularpeeker_elementHovered');
+                            });
+
+                            removeEventListeners();
+                        };
+
+                        var toggleActive = function () {
+                            if (!peekerActivated) {
+                                activatePeeker();
+                                logger.l('Peeker activated!');
+                            } else {
+                                deactivatePeeker();
+                                logger.l('Peeker dectivated!');
+                            }
+                            peekerActivated = !peekerActivated;
+                        };
+
+                        var displayScope = function (evt) {
+                            logger.l('Selected element source: ', evt.srcElement);
+                            logger.l('Selected scope: ', getScope(evt.srcElement));
+
+                            var selectedScope = getScope(evt.srcElement);
+
+                        };
+
+
+                        //===============================
+                        //      Private Constructor     =
+                        //===============================
+                        var Peeker = function () {
+                        };
+
+
+                        //===============================
+                        //      Private init            =
+                        //===============================
+                        $window.addEventListener('keydown', function (evt) {
+                            var keyCode = config.peekerHotKey.key.toUpperCase().charCodeAt(0);
+                            if ((evt.keyCode === keyCode) &&
+                                (evt.altKey === config.peekerHotKey.altKey) &&
+                                (evt.shiftKey === config.peekerHotKey.shiftKey) &&
+                                (evt.ctrlKey === config.peekerHotKey.ctrlKey)) {
+
+                                toggleActive();
+                            }
+                        }, false);
+
+                        // When peeker activetd
+                        $rootScope.$on('angularpeeker:run:peekeractivated', function () {
+
+                        });
+
+                        return new Peeker();
+                    }];
+            }]);
 
 }());
