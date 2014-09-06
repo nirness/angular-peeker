3e5a9c7aeb3ea68130cf8b1b5c99a32c7528c416
aviv <shaked.aviv1@gmail.com>
2014-09-06 16:53:00 +0300

    feat(peeker): Adds peeker functionality

--- app/scripts/run.js
+++ app/scripts/run.js
@@ -4,10 +4,12 @@ angular.module('angularPeeker')
         '$window',
         'config',
         'logger',
-        function ($rootScope, $window, config, logger) {
+        'peeker',
+        function ($rootScope, $window, config, logger, peeker) {
             logger.l('angularPeeker: run: $rootScope', $rootScope);
             logger.l('angularPeeker: run: $window', $window);
             logger.l('angularPeeker: run: config', config);
+            logger.l('angularPeeker: run: peeker', peeker);
 
             // Activates peeker on selected key
             $window.addEventListener('keydown', function (evt) {
@@ -25,8 +27,25 @@ angular.module('angularPeeker')
             // When peeker activetd
             $rootScope.$on('angularpeeker:run:peekeractivated', function () {
                 var body = document.getElementsByTagName('body')[0];
-                logger.l('Add class \'peekerActivated\' to body', body);
-                angular.element(body).addClass('peekerActivated');
+                var html = document.getElementsByTagName('html')[0];
+
+                logger.l('Add class \'angularpeeker_peekerActivated\' to html', body);
+                angular.element(html).addClass('angularpeeker_peekerActivated');
+
+                $window.addEventListener('mouseover', function (evt) {
+                    logger.l('Mouse over: ', evt);
+                    angular.element(evt.srcElement).addClass('angularpeeker_elementHovered');
+                }, true);
+
+                $window.addEventListener('mouseout', function (evt) {
+                    logger.l('Mouse out: ', evt);
+                    angular.element(evt.srcElement).removeClass('angularpeeker_elementHovered');
+                }, true);
+
+                $window.addEventListener('click', function (evt) {
+                    evt.stopImmediatePropagation();
+
+                }, true);
             });
         }
     ]);
