cac53e603c9c26c08981bb22eb9a3dae959ea803
aviv <shaked.aviv1@gmail.com>
2014-09-06 18:33:33 +0300

    refactor(config): Changes config to provider and adds methods to config angularPeeker

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -8,16 +8,37 @@
 angular.module('angularPeeker')
     .provider('config', [
         function () {
-            this.peekerHotKey = {
+            var peekerHotKey = {
                 key: 'p',
                 altKey: true,
                 shiftKey: false,
                 ctrlKey: true
             };
 
+            // Public API for configuration
+            this.setActivationKey = function (key) {
+                peekerHotKey.key = key;
+            };
+            this.setActivationKeyAlt = function (alt) {
+                peekerHotKey.altKey = alt;
+            };
+            this.setActivationKeyCtrl = function (ctrl) {
+                peekerHotKey.ctrlKey = ctrl;
+            };
+            this.setActivationKeyShift = function (shift) {
+                peekerHotKey.shiftKey = shift;
+            };
+            this.setActivationConfiguration = function (configObj) {
+                for (var key in configObj) {
+                    if (configObj.hasOwnProperty(key)) {
+                        peekerHotKey[key] = configObj[key];
+                    }
+                }
+            };
+
             this.$get = function () {
                 return {
-                    peekerHotKey: this.peekerHotKey
+                    peekerHotKey: peekerHotKey
                 };
             };
         }
