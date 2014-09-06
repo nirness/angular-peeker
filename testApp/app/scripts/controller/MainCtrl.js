f6f199d3f650f8046d6ba46962ab847794470008
aviv <shaked.aviv1@gmail.com>
2014-09-06 18:32:23 +0300

    test(testApp): Adds and displays a model in testApp

--- testApp/app/scripts/controller/MainCtrl.js
+++ testApp/app/scripts/controller/MainCtrl.js
@@ -2,6 +2,46 @@ angular.module('angularPeekerTest')
     .controller('MainCtrl', [
         '$scope',
         function ($scope) {
-            console.log('MainCtrl: $scope: ', $scope);
+            $scope.englishLeague = {
+                teams: [
+                    {
+                        name: 'Chelsea',
+                        matchesPlayed: 3,
+                        wins: 3,
+                        draws: 0,
+                        losses: 0,
+                        goalsFor: 11,
+                        goalsAgainst: 4
+                    },
+                    {
+                        name: 'Swansea City ',
+                        matchesPlayed: 3,
+                        wins: 3,
+                        draws: 0,
+                        losses: 0,
+                        goalsFor: 6,
+                        goalsAgainst: 1
+                    },
+                    {
+                        name: 'Aston Villa ',
+                        matchesPlayed: 3,
+                        wins: 2,
+                        draws: 1,
+                        losses: 0,
+                        goalsFor: 3,
+                        goalsAgainst: 1
+                    },
+                    {
+                        name: 'Manchester City ',
+                        matchesPlayed: 3,
+                        wins: 2,
+                        draws: 0,
+                        losses: 1,
+                        goalsFor: 5,
+                        goalsAgainst: 2
+                    }
+
+                ]
+            };
         }
     ]);
