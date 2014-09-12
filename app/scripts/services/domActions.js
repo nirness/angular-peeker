d8b82d0e8153bdbf18f35ac3e07d3cf4fa6cc40b
aviv <shaked.aviv1@gmail.com>
2014-09-12 17:37:20 +0300

    feat(domActions): Add factory that acts on the dom

---
+++ app/scripts/services/domActions.js
@@ -0,0 +1,144 @@
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name angularPeeker.domActions
+     * @description
+     * # domActions
+     * factory in the angularPeeker.
+     */
+    angular.module('angularPeeker')
+        .factory('domActions', function () {
+            var html = document.getElementsByTagName('html')[0],
+                body = document.getElementsByTagName('body')[0];
+
+
+            var factory = {
+
+                /**
+                 *
+                 * @param elems
+                 * @returns {string}
+                 */
+                getElementsType: function (elems) {
+                    if (elems instanceof Array) {
+                        return 'Array';
+                    } else if (elems instanceof NodeList) {
+                        return 'NodeList'
+                    } else if (elems instanceof Node) {
+                        return 'Node'
+                    } else if (elems.scope && elems.injector && elems.length !== undefined) {
+                        return 'angular'
+                    } else if (typeof elems === 'string') {
+                        return 'string'
+                    }
+                },
+                convertElemsToArray: {
+                    /**
+                     *
+                     * @param elems {Array}
+                     * @returns {*}
+                     */
+                    'Array': function (elems) {
+                        return elems;
+                    },
+                    /**
+                     *
+                     * @param elems {NodeList}
+                     * @returns {*|Array.<T>}
+                     */
+                    'NodeList': function (elems) {
+                        return Array.prototype.slice.call(elems);
+                    },
+                    /**
+                     *
+                     * @param elem {Node}
+                     * @returns {Array|[]}
+                     */
+                    'Node': function (elem) {
+                        return [elem]
+                    },
+                    /**
+                     *
+                     * @param elems
+                     * @returns {Array|[]}
+                     */
+                    'angular': function (elems) {
+                        var length = elems.length,
+                            arr = [],
+                            i;
+
+                        for (i = 0; i < length; i += 1) {
+                            arr.push(elems[i]);
+                        }
+
+                        return arr;
+                    },
+                    /**
+                     *
+                     * @param str
+                     * @returns {Array|[]}
+                     */
+                    'string': function (str) {
+                        var arr = [],
+                            elm;
+                        elm = angular.element(str);
+
+                        if (elm.length) {
+                            arr.push(elm);
+                        }
+
+                        return arr;
+                    }
+                },
+                /**
+                 *
+                 * @param selector {string} The selector string
+                 * @param element {*}
+                 * @returns {Array} array of node elements
+                 */
+                find: function (selector, element) {
+                    // Set element as received or as html default
+                    element = element || html;
+                    var elementsArr = [];
+
+                    // Find the element type
+                    var elmType = factory.getElementsType(element);
+                    // Convert the element to array
+                    var arr = factory.convertElemsToArray[elmType](element);
+
+                    // Iterate through the array
+                    arr.forEach(function (member) {
+                        // Parse onle Node elements
+                        if (factory.getElementsType(member) === 'Node') {
+                            // Find elements by selector on member
+                            var elements = member.querySelectorAll(selector);
+
+                            if (elements) {
+                                // Turn elements into array
+                                elements = Array.prototype.slice.call(elements);
+                                // Push all elements in array to the returned array
+                                elements.forEach(function (elem) {
+                                    elementsArr.push(elem);
+                                });
+                            }
+                        }
+                    });
+
+                    return elementsArr
+                }
+            };
+
+            return factory;
+        });
+
+    // remove all angularpeeker_elementHovered classes from dom
+    //var elements = html.querySelectorAll('.angularpeeker_elementHovered');
+    //if (elements) {
+    //    elements = Array.prototype.slice.call(elements);
+    //    elements.forEach(function (ele) {
+    //        angular.element(ele).removeClass('angularpeeker_elementHovered');
+    //    });
+    //}
+}());
