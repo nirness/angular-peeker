0a614a0afcf27d4558294a7b188c33cba87b231e
aviv <shaked.aviv1@gmail.com>
2014-09-12 12:36:10 +0300

    feat(toggleScopeView): Adds toggle scope view logic

--- app/scripts/directives/peekerStrip.js
+++ app/scripts/directives/peekerStrip.js
@@ -1,17 +1,37 @@
 angular.module('angularPeeker')
     .directive('peekerStrip', [
+        '$rootScope',
         '$timeout',
-        function ($timeout) {
+        function ($rootScope, $timeout) {
             return {
                 restrict: 'E',
                 templateUrl: 'peeker-strip.html',
                 controller: function ($scope, $element, $attrs) {
+                    // Init
+                    $scope.scopeViewerState = 'Hide';
+                    $scope.scopeViewerActive = false;
+
+                    $scope.toggleShowHideText = function () {
+                        $scope.scopeViewerState = $scope.scopeViewerState === 'Hide' ? 'Show' : 'Hide';
+                    };
+
+                    $scope.toggleShowHideState = function () {
+                        $rootScope.$broadcast('angularpeeker:peekerstrip:requesttogglestate', $scope.scopeViewerState);
+                        $scope.toggleShowHideText();
+                    };
+
                     $scope.removeStrip = function () {
                         angular.element($element[0].querySelector('.peeker_strip')).addClass('underBottom').removeClass('bottom');
                         $timeout(function () {
                             $element.remove();
                         }, 1200);
                     };
+
+
+                    //Setup scope listeners
+                    $scope.$on('angularpeeker:peeker:scopevieweractive', function () {
+                        $scope.scopeViewerActive = true;
+                    });
 
                     $scope.$on('angularpeeker:peeker:peekerdeactivated', function () {
                         $scope.removeStrip();
@@ -21,6 +41,7 @@ angular.module('angularPeeker')
                     setTimeout(function () {
                         angular.element(element[0].querySelector('.peeker_strip')).addClass('bottom').removeClass('underBottom');
                     }, 200);
+
                 }
             };
         }]);
--- app/scripts/directives/scopeViewer.js
+++ app/scripts/directives/scopeViewer.js
@@ -2,7 +2,7 @@ angular.module('angularPeeker')
     .directive('scopeViewer', [
         'scopeViewBuilder',
         function (scopeViewBuilder) {
-            var doc;
+            var viewer;
 
 
             return {
@@ -12,12 +12,51 @@ angular.module('angularPeeker')
 
                 },
                 link: function (scope, element) {
-                    var link = scopeViewBuilder.createDisplayModel(
-                        scope.selectedScope,
-                        null,
-                        'selectedScope');
-                    doc = link(scope);
-                    element[0].querySelector('.angular_peeker_container').appendChild(doc[0]);
+
+
+                    // scope methods
+                    //==============
+                    scope.killViewer = function () {
+                        element[0].querySelector('.angular_peeker_container').removeChild(viewer[0]);
+                        viewer = null;
+                    };
+
+                    scope.appendViewer = function () {
+                        element[0].querySelector('.angular_peeker_container').appendChild(viewer[0]);
+                    };
+
+                    scope.hideElement = function () {
+                        element[0].style.display = 'none';
+                    };
+
+                    scope.showElement = function () {
+                        element[0].attributes.removeNamedItem('style');
+                    };
+
+
+                    // Init
+                    //=======
+
+                    // Build the scope view and get a link function
+                    var link = scopeViewBuilder.buildScopeView(
+                        scope.selectedScope, // The selected scope
+                        null, // No original doc
+                        'selectedScope' // The name of the original property
+                    );
+                    // Run the link function
+                    viewer = link(scope);
+                    // Append the result document to angular_peeker_container
+                    scope.appendViewer();
+
+
+                    // Set scope listeners
+                    scope.$on('angularpeeker:peekerstrip:requesttogglestate', function (evt, data) {
+                        if (data.toLowerCase() === 'hide') {
+                            scope.hideElement();
+                        } else {
+                            scope.showElement();
+                        }
+                    });
                 }
             };
         }]);
