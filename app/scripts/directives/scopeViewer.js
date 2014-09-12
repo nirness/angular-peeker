9080a0511226a18fdccc96e998a32492c24d95b3
aviv <shaked.aviv1@gmail.com>
2014-09-12 23:06:01 +0300

    chore(scopeViewer): Surrounds in closure.

--- app/scripts/directives/scopeViewer.js
+++ app/scripts/directives/scopeViewer.js
@@ -1,70 +1,73 @@
-angular.module('angularPeeker')
-    .directive('scopeViewer', [
-        'scopeViewBuilder',
-        function (scopeViewBuilder) {
-            var viewer;
+(function () {
+    'use strict';
+
+    angular.module('angularPeeker')
+        .directive('scopeViewer', [
+            'scopeViewBuilder',
+            function (scopeViewBuilder) {
+                var viewer;
+
+                return {
+                    restrict: 'E',
+                    templateUrl: 'scopeViewer.html',
+                    controller: function ($scope, $element, $attrs) {
+
+                        // Cleanup
+                        $element.on('$destroy', function (evt) {
+                            $scope = null;
+                        });
+                    },
+                    link: function (scope, element) {
 
 
-            return {
-                restrict: 'E',
-                templateUrl: 'scopeViewer.html',
-                controller: function ($scope, $element, $attrs) {
+                        // scope methods
+                        //==============
+                        scope.destroyViewer = function () {
+                            element.remove();
+                            viewer = null;
+                        };
 
-                    // Cleanup
-                    $element.on('$destroy', function (evt) {
-                        $scope = null;
-                    });
-                },
-                link: function (scope, element) {
+                        scope.appendViewer = function () {
+                            element[0].querySelector('.angular_peeker_container').appendChild(viewer[0]);
+                        };
 
+                        scope.hideElement = function () {
+                            element[0].style.display = 'none';
+                        };
 
-                    // scope methods
-                    //==============
-                    scope.destroyViewer = function () {
-                        element.remove();
-                        viewer = null;
-                    };
-
-                    scope.appendViewer = function () {
-                        element[0].querySelector('.angular_peeker_container').appendChild(viewer[0]);
-                    };
-
-                    scope.hideElement = function () {
-                        element[0].style.display = 'none';
-                    };
-
-                    scope.showElement = function () {
-                        element[0].attributes.removeNamedItem('style');
-                    };
+                        scope.showElement = function () {
+                            element[0].attributes.removeNamedItem('style');
+                        };
 
 
-                    // Init
-                    //=======
+                        // Init
+                        //=======
 
-                    // Build the scope view and get a link function
-                    var link = scopeViewBuilder.buildScopeView(
-                        scope.selectedScope, // The selected scope
-                        null, // No original doc
-                        'selectedScope' // The name of the original property
-                    );
-                    // Run the link function
-                    viewer = link(scope);
-                    // Append the result document to angular_peeker_container
-                    scope.appendViewer();
+                        // Build the scope view and get a link function
+                        var link = scopeViewBuilder.buildScopeView(
+                            scope.selectedScope, // The selected scope
+                            null, // No original doc
+                            'selectedScope' // The name of the original property
+                        );
+                        // Run the link function
+                        viewer = link(scope);
+                        // Append the result document to angular_peeker_container
+                        scope.appendViewer();
 
 
-                    // Set scope listeners
-                    scope.$on('angularpeeker:peekerstrip:requesttogglestate', function (evt, data) {
-                        if (data.toLowerCase() === 'hide') {
-                            scope.hideElement();
-                        } else {
-                            scope.showElement();
-                        }
-                    });
+                        // Set scope listeners
+                        scope.$on('angularpeeker:peekerstrip:requesttogglestate', function (evt, data) {
+                            if (data.toLowerCase() === 'hide') {
+                                scope.hideElement();
+                            } else {
+                                scope.showElement();
+                            }
+                        });
 
-                    scope.$on('angularpeeker:peekerstrip:requestdestroyviewer', function () {
-                        scope.destroyViewer();
-                    });
-                }
-            };
-        }]);
+                        scope.$on('angularpeeker:peekerstrip:requestdestroyviewer', function () {
+                            scope.destroyViewer();
+                        });
+                    }
+                };
+            }]);
+}());
